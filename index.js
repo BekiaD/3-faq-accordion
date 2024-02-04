ff5bb12daa97c9a6cc949d384c356d9859051440
BekiaD <144695091+BekiaD@users.noreply.github.com>
2024-02-04 17:37:59 +0100

    js start

---
+++ index.js
@@ -0,0 +1,8 @@
+// What do i want to do?
+// - Set the 'relax' state to closed accordion (step )
+// - When the button or the title is clicked, open up the answer (step )
+// - in the same time change the button from + - (step )
+// - when clicked on the minus or the title again, fold the text up 
+// - and switch the button back (step )
+
+// --
